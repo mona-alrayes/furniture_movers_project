afcee570641ff5f6338cb1b411c4349283ab5fad
Mona Alrayes <=>
2025-07-17 00:22:15 +0300

    'adding core folder , theme with colors and fonts files , and adding controllers folders for screens'

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF4B75CB); //done
+  static const secondary = Color(0xFFF5904C); // done
+  static const mediumPrimary = Color(0xFFAECDFB); //done
+  static const lightPrimary = Color(0xFFCCE0FB);  //done
+  static const black = Color(0xFF1E1E1E); //done
+  static const lightGrey = Color(0xFFF8F9FA); //done
+  static const red = Color(0xFFFF0000);  //done
+  static const grey = Color(0xFF98A2B3); //done
+  static const regularGrey = Color(0xFFDEE2E6);  //done
+  static const mediumGrey = Color(0xFFCED4DA); //done
+  static const lightPrimaryGrey = Color(0xFFF0F6FE); //done
+  static const white = Color(0xFFFFFFFF);  //done
+  static const green = Color(0xFF1FC61F);  // done
+  static const yellow = Color(0xFFFFC740);  //done
+  static const veryLightGrey = Color(0xFFFAFBFB);
+  static const darkBlue = Color(0xFF00246D); //done
+  static const brightBlue = Color(0xFF00B5EE); //done
+  static const LinearGradient linears = LinearGradient(
+    colors: [
+      Color(0xFF4B75CB), 
+      Color(0xFF4999CB), 
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+}
