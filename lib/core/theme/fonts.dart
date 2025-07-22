a738eeb18b460f3a1eae4289be297dad57031806
Mona Alrayes <=>
2025-07-22 22:13:30 +0300

    'fix main and fonts made static and google fonts ones for chat'

--- lib/core/theme/fonts.dart
+++ lib/core/theme/fonts.dart
@@ -131,40 +131,55 @@ class AppFonts {
 
 
   //chat screens fonts
+  // Chat screen fonts (now consistent with the rest)
 
-  static TextStyle lastMessagesChatFont = GoogleFonts.almarai(
-    fontSize: 12.sp,
-    fontWeight: FontWeight.w700,
-    color: AppColors.grey,
-  );
-  static TextStyle mainNameChatMessagesFont = GoogleFonts.almarai(
-    fontSize: 14.sp,
-    fontWeight: FontWeight.w700,
-    color: AppColors.paltteBlack,
-  );
-  static TextStyle statueConectionChatMessagesFont = GoogleFonts.almarai(
-    fontSize: 12.sp,
-    fontWeight: FontWeight.w400,
-    color: AppColors.green,
-  );
-  static TextStyle daySentMessagesFont = GoogleFonts.almarai(
-    fontSize: 12.sp,
-    fontWeight: FontWeight.w700,
-    color: AppColors.black,
-  );
-  static TextStyle userMessagesFont = GoogleFonts.poppins(
-    fontSize: 14.sp,
-    fontWeight: FontWeight.w300,
-    color: AppColors.white,
-  );
-  static TextStyle formatTimeMessagesFont = GoogleFonts.poppins(
-    fontSize: 10.sp,
-    fontWeight: FontWeight.w500,
-    color: AppColors.grey,
-  );
-  static TextStyle hintTextBoxFont = GoogleFonts.poppins(
-    fontSize: 12.sp,
-    fontWeight: FontWeight.w400,
-    color: AppColors.grey,
-  );
+static TextStyle lastMessagesChatFont = TextStyle(
+  fontFamily: 'Almarai',
+  fontSize: 12.sp,
+  fontWeight: FontWeight.w700,
+  color: AppColors.grey,
+);
+
+static TextStyle mainNameChatMessagesFont = TextStyle(
+  fontFamily: 'Almarai',
+  fontSize: 14.sp,
+  fontWeight: FontWeight.w700,
+  color: AppColors.paltteBlack,
+);
+
+static TextStyle statueConectionChatMessagesFont = TextStyle(
+  fontFamily: 'Almarai',
+  fontSize: 12.sp,
+  fontWeight: FontWeight.w400,
+  color: AppColors.green,
+);
+
+static TextStyle daySentMessagesFont = TextStyle(
+  fontFamily: 'Almarai',
+  fontSize: 12.sp,
+  fontWeight: FontWeight.w700,
+  color: AppColors.black,
+);
+
+static TextStyle userMessagesFont = TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 14.sp,
+  fontWeight: FontWeight.w300,
+  color: AppColors.white,
+);
+
+static TextStyle formatTimeMessagesFont = TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 10.sp,
+  fontWeight: FontWeight.w500,
+  color: AppColors.grey,
+);
+
+static TextStyle hintTextBoxFont = TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 12.sp,
+  fontWeight: FontWeight.w400,
+  color: AppColors.grey,
+);
+
 }
